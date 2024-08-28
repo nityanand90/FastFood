c75f87d1a45892c9f55eb2a683776cc1e0f97d46
nityanand90 <160598872+nityanand90@users.noreply.github.com>
2024-08-29 01:52:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,170 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Pizza from './images/pizza.jpg'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <div className='container'>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='product-image'>
+          <img src={Pizza} />
+          <h3>Pizza</h3>
+          <p>This is very taste food try it.</p>
+          <span>40₹</span>
+          <div>
+          <button className='btn'>Add To Card </button><br></br>
+          <button className='btn'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+      </div>
+      <Footer />
+
+
+
+    </div>
+  )
+}
+
+export default App
+
